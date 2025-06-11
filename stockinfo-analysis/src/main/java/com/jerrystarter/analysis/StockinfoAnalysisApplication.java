c558fdc8cb75ea5e67d1a032dd08bc1d7500cde3
JeRRy <lony1107@gmail.com>
2025-06-11 13:59:48 +0800

    MOD: stockinfo is running

---
+++ stockinfo-analysis/src/main/java/com/jerrystarter/analysis/StockinfoAnalysisApplication.java
@@ -0,0 +1,11 @@
+package com.jerrystarter.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockinfoAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockinfoAnalysisApplication.class, args);
+    }
+}
